52cc3e6c9b396a22114dbeec4bcc40ecf521ecc9
anisgodev <31368230+anisgodev@users.noreply.github.com>
2023-06-25 19:57:05 +0200

    Delete jhi directory

--- jhi/src/main/java/com/bnppf/alphacredit/creditproductdesigner/repository/AuthorityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bnppf.alphacredit.creditproductdesigner.repository;
-
-import com.bnppf.alphacredit.creditproductdesigner.domain.Authority;
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-
-/**
- * Spring Data R2DBC repository for the {@link Authority} entity.
- */
-public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
